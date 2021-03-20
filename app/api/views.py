8e8de253d691c6573cde7dc13371e61a88a2bd91
Kirill Maiboroda <kirill.maiboroda@mail.ru>
2021-03-20 13:19:47 +0300

    Add files via upload

--- app/api/views.py
+++ app/api/views.py
@@ -5,9 +5,34 @@ from rest_framework.decorators import action, api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .services import CasinoService
+import requests
+
+
+BASE_URL = 'https://api.telegram.org/bot1682503641:AAEXVqQYzuox0rlOSkENYw_4n91BgZnPYfE/'
+
+
+def send_message(chat_id, text):
+    url = BASE_URL + 'sendMessage'
+    answer = {'chat_id': chat_id, 'text': text}
+    r = requests.post(url, json=answer)
+    return r.json()
 
 
 @api_view(['POST'])
 def get_webhook(request):
-    print(request.data)
+    data = request.data
+    chat_id = data['message']['chat']['id']
+    message = data['message']['text']
+    name = data['from']['first_name']
+
+    text = f'Hello {name} welcome to our casino!'
+
+    if 'casino' in message:
+        send_message(chat_id, text)
     return Response('hello!')
+
+
+# https://telegram-casino.herokuapp.com/api/v1/webhook/
+
+
+# https://api.telegram.org/bot1682503641:AAEXVqQYzuox0rlOSkENYw_4n91BgZnPYfE/setWebhook?url=telegram-casino.herokuapp.com/api/v1/webhook/
